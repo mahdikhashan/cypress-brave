77f082947c03067272554b5897849a9842c16dba
Mahdi Khashan <mahdikhashan1@gmail.com>
2022-10-08 00:09:40 +0330

    feat: setup project

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+/**
+ * A function that returns an object with Brave browser path
+ * */
+type braveBrowser = () => {}
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import execa from "execa"
+
+const braveBrowser = () => {
+    // the path is hard-coded for simplicity
+    const browserPath =
+        '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
+
+    return execa(browserPath, ['--version']).then((result) => {
+        // STDOUT will be like "Brave Browser 77.0.69.135"
+        const [, version] = /Brave Browser (\d+\.\d+\.\d+\.\d+)/.exec(result.stdout)
+        const majorVersion = parseInt(version.split('.')[0])
+
+        return {
+            name: 'Brave',
+            channel: 'stable',
+            family: 'chromium',
+            displayName: 'Brave',
+            version,
+            path: browserPath,
+            majorVersion,
+        }
+    })
+}
+
+export default braveBrowser;
